6642ee8dec3412e8779bc91fea3acb599b68b33a
SkywardSpectrum <151830380+SkywardSpectrum@users.noreply.github.com>
2024-07-29 16:17:32 +0530

    first commit

---
+++ build/sjtu_drone_description/CMakeFiles/plugin_drone.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/nambiar/Autonomous_Drone/src/drone_model/sjtu_drone_description/plugins/libplugin_drone.pdb"
+  "/home/nambiar/Autonomous_Drone/src/drone_model/sjtu_drone_description/plugins/libplugin_drone.so"
+  "CMakeFiles/plugin_drone.dir/src/pid_controller.cpp.o"
+  "CMakeFiles/plugin_drone.dir/src/pid_controller.cpp.o.d"
+  "CMakeFiles/plugin_drone.dir/src/plugin_drone.cpp.o"
+  "CMakeFiles/plugin_drone.dir/src/plugin_drone.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/plugin_drone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
